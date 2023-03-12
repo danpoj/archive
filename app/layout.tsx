173ce621db9eb2a36b6f0b1845963344aacffa17
danpoj <jws970306@khu.ac.kr>
2023-03-12 15:20:55 +0900

    default layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='ko'>
-      <body>
+    <html lang='ko' className='antialiased'>
+      <body className='antialiased'>
         {children}
         <Toaster />
       </body>
